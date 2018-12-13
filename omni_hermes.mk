c9554c5c6cf3262fd5f9c02b08fa13d3af1f417f
INFERNOcr <41138638+INFERNOcr@users.noreply.github.com>
2018-12-13 19:12:14 +0700

    Update omni_hermes.mk

--- omni_hermes.mk
+++ omni_hermes.mk
@@ -1,28 +1,24 @@
-#
-# Copyright (C) 2017 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+DEVICE_PATH := device/xiaomi/hermes
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Get the gsm config
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from hermes device
-$(call inherit-product, device/xiaomi/hermes/device.mk)
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := hermes
+
+# Device display
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_DEVICE := hermes
 PRODUCT_NAME := omni_hermes
@@ -31,6 +27,6 @@ PRODUCT_MODEL := Redmi Note 2
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-#BUILD_FINGERPRINT := "Xiaomi/hermes/hermes:5.0.2/LRX22G/V9.6.3.0.LHMMIFD:user/release-keys"
+#BUILD_FINGERPRINT := "Xiaomi/hermes/hermes:5.0.2/LRX22G/V9.5.4.0.LHMMIFA:user/release-keys"
 #PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="hermes-user 5.0.2 LRX22G V9.6.3.0.LHMMIFD release-keys"
+#    PRIVATE_BUILD_DESC="hermes-user 5.0.2 LRX22G V9.5.4.0.LHMMIFA release-keys"
